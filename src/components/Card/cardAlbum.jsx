a39945fefae5ec5d5f1fc6d83c418f0027573582
joce1112 <180935@utags.edu.mx>
2022-02-13 23:51:34 -0600

    Bug fix deployed

--- src/components/Card/cardAlbum.jsx
+++ src/components/Card/cardAlbum.jsx
@@ -1,4 +1,3 @@
-import { useTheme } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -7,7 +6,6 @@ import * as React from 'react';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 
 export default function CardAlbum({ name, image,artist,url}) {
-  const theme = useTheme();
   return(
     <Card sx={{ maxWidth: 345 }}>
     <CardActionArea>
